3f9c33de8d2e0b78e6f7311476e27c8fd449e06d
N00byEdge <hannesbredberg@gmail.com>
2021-01-06 15:50:53 +0100

    Initial work on pinephone support

---
+++ src/platform/pine_aarch64/entry.asm
@@ -0,0 +1,64 @@
+.global _start
+
+.section .text.entry
+_start:
+  // Disable interrupts
+  MSR DAIFSET, 0xF
+
+  // Disable paging and cache
+  MRS X0, SCTLR_EL1
+  AND X0, X0, ~3
+  MSR SCTLR_EL1, X0
+
+  MOV X0, 0x41000000
+  MOV SP, X0
+
+  // Get memory layout from u-boot script
+  MOV X0, 0x40800000
+  LDP X1, X2, [X0, #0x00]
+  ADR X5, dtb_loc
+  STP X1, X2, [X5, #0x00]
+
+  LDP X1, X2, [X0, #0x10]
+  STR X1, [X5, kernel_file_loc - dtb_loc]
+
+  // Next usable physmem
+  ADD X2, X2, X1
+  ADR X5, pmm_head
+  STR X2, [X5]
+
+  B _main
+
+.section .data
+
+.global devicetree_tag
+.balign 16
+devicetree_tag:
+  .8byte 0xabb29bd49a2833fa // DeviceTree
+  .8byte 0
+dtb_loc:
+  .8byte 0
+  .8byte 0
+
+.global uart_tag
+.global uart_reg
+.global uart_status
+.global uart_status_mask
+.global uart_status_value
+.balign 8
+uart_tag:
+  .8byte 0xf77485dbfeb260f9 // u32 MMIO UART with status
+  .8byte 0
+uart_reg:
+  .8byte __uart_base + 0x00 // TX Holding Register
+uart_status:
+  .8byte __uart_base + 0x14 // Line Status Register
+uart_status_mask:
+  .4byte 0x00000040 // TX Holding Register Empty
+uart_status_value:
+  .4byte 0x00000040 // TX Holding Register Empty is set
+
+.global kernel_file_loc
+.balign 8
+kernel_file_loc:
+  .8byte 0
---
+++ src/platform/pine_aarch64/main.zig
@@ -0,0 +1,46 @@
+pub const sabaton = @import("../../sabaton.zig");
+pub const io = sabaton.io_impl.status_uart_mmio_32;
+pub const ElfType = [*]u8;
+pub const panic = sabaton.panic;
+
+const std = @import("std");
+
+// We know the page size is 0x1000
+pub fn get_page_size() u64 {
+  return 0x1000;
+}
+
+export fn _main() noreturn {
+  //sabaton.paging.detect_page_size();
+  @call(.{.modifier = .always_inline}, sabaton.main, .{});
+}
+
+pub fn get_kernel() ElfType {
+  return sabaton.near("kernel_file_loc").read([*]u8);
+}
+
+// pub fn get_dtb() []u8 {
+//   return sabaton.near("dram_base").read([*]u8)[0..0x100000];
+// }
+
+pub fn get_dram() []u8 {
+  return sabaton.near("dram_base").read([*]u8)[0..get_dram_size()];
+}
+
+fn get_dram_size() u64 {
+  return 0x80000000;
+}
+
+pub fn map_platform(root: *sabaton.paging.Root) void {
+  const uart_base = sabaton.near("uart_reg").read(u64);
+  sabaton.paging.map(uart_base, uart_base, 0x1000, .rw, .mmio, root, .CannotOverlap);
+
+  const blob_base = @ptrToInt(sabaton.near("__blob_base").addr(u8));
+  const blob_end  = @ptrToInt(sabaton.near("__blob_end").addr(u8));
+  sabaton.paging.map(blob_base, blob_base, blob_end - blob_base, .rwx, .memory, root, .CannotOverlap);
+}
+
+pub fn add_platform_tags(kernel_header: *sabaton.Stivale2hdr) void {
+  sabaton.add_tag(&sabaton.near("uart_tag").addr(sabaton.Stivale2tag)[0]);
+  sabaton.add_tag(&sabaton.near("devicetree_tag").addr(sabaton.Stivale2tag)[0]);
+}
