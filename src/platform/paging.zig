f1bf5af2645d2776eab17c71728003f88595c584
N00byEdge <hannesbredberg@gmail.com>
2021-01-06 14:54:13 +0100

    Paging: Flush any page tables when applying paging

--- src/platform/paging.zig
+++ src/platform/paging.zig
@@ -242,6 +242,8 @@ pub fn apply_paging(r: *Root) void {
     \\MSR SCTLR_EL1, %[sctlr]
     \\DSB SY
     \\ISB SY
+    \\paging_flush:
+    \\TLBI ALLE1
     :
     : [ttbr0] "r" (r.ttbr0)
     , [ttbr1] "r" (r.ttbr1)
