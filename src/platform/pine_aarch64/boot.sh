01758e846758dfb0a47805a79a5adbc7134c6033
N00byEdge <hannesbredberg@gmail.com>
2021-02-27 20:26:19 +0100

    Add boot script messages

--- src/platform/pine_aarch64/boot.sh
+++ src/platform/pine_aarch64/boot.sh
@@ -1,10 +1,13 @@
 setenv autostart no
 
+echo "[SABATON] Setting up identity map"
 load mmc "1:2" 80000000 /identity
 go 80000000
 
+echo "[SABATON] Loading Sabaton"
 load mmc "1:2" 40000000 /Sabaton.bin
 
+echo "[SABATON] Loading DTB"
 setexpr load_addr 40020000
 mw.l 40000010 ${load_addr}
 load mmc "1:4" ${load_addr} /dtb
@@ -14,6 +17,7 @@ setexpr load_addr ${load_addr} '+' ${filesize}
 setexpr load_addr ${load_addr} '+' 00000fff
 setexpr load_addr ${load_addr} '&' fffff000
 
+echo "[SABATON] Loading your kernel"
 mw.l 40000020 ${load_addr}
 load mmc "1:9" ${load_addr} /Kernel.elf
 
@@ -21,5 +25,6 @@ setexpr load_addr ${load_addr} '+' ${filesize}
 setexpr load_addr ${load_addr} '+' 00000fff
 setexpr load_addr ${load_addr} '&' fffff000
 
+echo "[SABATON] Starting Sabaton..."
 mw.l 40000028 ${load_addr}
 go 40000030
