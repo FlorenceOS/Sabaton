6a56d3ddb63bf1d9d6c30611aa37d7d39cc38144
N00byEdge <me@hannesbredberg.se>
2021-03-06 23:44:41 +0100

    Pine: Don't load files at a constant offset

--- src/platform/pine_aarch64/boot.sh
+++ src/platform/pine_aarch64/boot.sh
@@ -1,14 +1,18 @@
 setenv autostart no
 
 echo "[SABATON] Setting up identity map"
-load mmc "1:2" 80000000 /identity
+load mmc "1:4" 80000000 /identity
 go 80000000
 
+setexpr load_addr 40000000
 echo "[SABATON] Loading Sabaton"
-load mmc "1:2" 40000000 /Sabaton.bin
+load mmc "1:4" ${load_addr} /Sabaton.bin
+
+setexpr load_addr ${load_addr} '+' ${filesize}
+setexpr load_addr ${load_addr} '+' 00000fff
+setexpr load_addr ${load_addr} '&' fffff000
 
 echo "[SABATON] Loading DTB"
-setexpr load_addr 40020000
 mw.l 40000010 ${load_addr}
 load mmc "1:4" ${load_addr} /dtb
 mw.l 40000018 ${filesize}
