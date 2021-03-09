75cd794aa7c31c82cd7621a50546c47bb323a268
N00byEdge <hannesbredberg@gmail.com>
2021-03-10 00:10:46 +0100

    Virt display: Break out into own file

--- src/platform/virt_aarch64/main.zig
+++ src/platform/virt_aarch64/main.zig
@@ -5,25 +5,7 @@ pub const panic = sabaton.panic;
 
 const std = @import("std");
 
-pub const display = struct {
-  fn try_find(comptime f: anytype, comptime name: []const u8) bool {
-    const retval = f();
-    if(retval) {
-      sabaton.puts("Found " ++ name ++ "!\n");
-    } else {
-      sabaton.puts("Couldn't find " ++ name ++ "\n");
-    }
-    return retval;
-  }
-
-  pub fn init() void {
-    // First, try to find a ramfb
-    if(try_find(sabaton.ramfb.init, "ramfb"))
-      return;
-
-    sabaton.puts("Kernel requested framebuffer but we could not provide one!\n");
-  }
-};
+pub const display = @import("display.zig");
 
 pub const acpi = struct {
   pub fn init() void {
